5d718df6745eb248a448e0971fc19c107492fb8a
KaiserJagger <kaiserjager10@gmail.com>
2024-07-04 12:58:03 -0300

    Node express TS, Jest and Eslint ready

---
+++ __tests__/example.spec.ts
@@ -0,0 +1,6 @@
+describe('My first test suite', () => {
+
+    it('My First test case', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ app.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
+
+// Define routes
+app.get('/', (req: Request, res: Response) => {
+    res.send('APP Express + TS + Nodemon + Swagger + Mongoose');
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+    res.send('Hello world');
+});
+
+
+
+app.listen(port, () => {
+     console.log(`EXPRESS SERVER: Running at http://localhost:${port}`)
+});
